c045af4cd7bd661ac59ef139f64c03beb58521a3
Abubaker Targoni <abubakertargoni@hotmail.com>
2025-06-25 15:30:08 +0100

    Attempted to add File Transfer - unsuccessful

--- ChatClient/obj/Debug/net9.0/ChatClient.AssemblyInfo.cs
+++ ChatClient/obj/Debug/net9.0/ChatClient.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d2a623c0c0409075242e141675e8a4b8dc266ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34aaca7ea4ec106fca674660513c1ce046772942")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
